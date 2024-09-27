67c1b32032d110f196ca538c86ab6e8a86abbfed
truckdrivercz <eda.orvos@seznam.cz>
2024-09-27 09:27:57 +0200

    update

---
+++ res/js/main.js
@@ -0,0 +1,30 @@
+let frog = document.getElementById("frog");
+
+window.onload = () => {
+    moveToFistLily();
+}
+
+let moveToFistLily = () => {
+    frog.style.left = 50 + "%";
+    frog.style.top = 650 + "px";
+}
+
+let moveToSecondLily = () => {
+    frog.style.left = 40 + "%";
+    frog.style.top = 550 + "px";
+}
+
+let moveToThirdList = () =>{
+    frog.style.left = 60 + "%";
+    frog.style.top = 450 + "px";
+}
+
+let moveToFourthLily = () => {
+    frog.style.left = 40 + "%";
+    frog.style.top = 350 + "px";
+}
+
+let moveToFifthLily = () => {
+    frog.style.left = 60 + "%";
+    frog.style.top = 250 + "px"
+}
